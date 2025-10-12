73833581133dd7380ff1a94bfe2b00bfae5ff9e4
yngcmryl <yungcoriggy@gmail.com>
2025-10-12 22:46:48 +0800

    Updated coach_dashboard

--- coach/urls.py
+++ coach/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views 
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.coach_dashboard, name='dashboard'),
     path('register/', views.register_view, name='register'),   
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
