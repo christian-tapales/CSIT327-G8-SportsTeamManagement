91de55f8556dc8e1cfbf6269744b08c236567f29
FrancesLgheUnabia <lghefranz@gmail.com>
2025-10-14 02:46:27 +0800

    temp: save urls update before switching branch

--- team_mgmt/urls.py
+++ team_mgmt/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include  # include lets you reference app URLs
-from django.views.generic import RedirectView
+from django.urls import path, include
+from coach import views  # import your landing page view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('coach/', include('coach.urls')),  # Routes /coach/ to coach app URLs
-    path('', RedirectView.as_view(url='/coach/', permanent=False)),
+    path('coach/', include('coach.urls')),
+    path('', views.landing_page, name='landing_page'),  # 👈 new default route
 ]
