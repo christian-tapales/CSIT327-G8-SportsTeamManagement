633e09696f6743e9aabe63ed6f4830357e520d56
YlanDainne <abeliosoldano@gmail.com>
2025-12-09 23:38:23 +0800

    Fix media file upload issue and update settings

--- team_mgmt/urls.py
+++ team_mgmt/urls.py
@@ -23,3 +23,9 @@ urlpatterns = [
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Force serve media files in production (Use with caution, for simple deployments)
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
